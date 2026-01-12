6542b0e32d0fddd3faff1ef5bbdbf9503d943475
shahwar-coder <naqvishahwar120@gmail.com>
2026-01-12 13:00:37 +0530

    1st file - decorator

---
+++ Python/Decorators/01_Decorators_:_Login_protection.py
@@ -0,0 +1,24 @@
+'''
+1️⃣ Authentication – Orders Page
+You have an API handler function:
+
+def view_orders(user, is_logged_in):
+    return "Orders data"
+
+Task:
+Create a decorator login_required that allows this function to run only if is_logged_in=True. Otherwise, return "Please login to continue".    
+'''
+
+def login_required(func):
+    def wrapper(user, is_logged_in):
+        if is_logged_in:
+            return func(user, is_logged_in)
+        else:
+            return "Please login to continue"
+    return wrapper
+
+@login_required
+def view_orders(user, is_logged_in):
+    return "Orders Data"
+
+print(view_orders("Shahwar", True))
